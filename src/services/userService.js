eff41dc7146a771463d0448bd59305fc0a1d6547
MUHAMMAD HASSAN <itsmaniwaseem5@gmail.com>
2025-08-18 10:16:16 +0500

    pass hash

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,27 +1,35 @@
-const UserRepository = require('../repositories/userRepository');
+const bcrypt = require("bcrypt");
+const UserRepository = require("../repositories/userRepository");
 const userRepo = new UserRepository();
 
 class UserService {
   async createUser(data) {
+    // password ko hash karo
+    const saltRounds = 10;
+    data.password = await bcrypt.hash(data.password, saltRounds);
+
     return await userRepo.create(data);
   }
 
   async findAll() {
-  return await userRepo.find({ where: { isDeleted: false } });
-}
-
+    return await userRepo.find({ where: { isDeleted: false } });
+  }
 
   async getUserById(id) {
     return await userRepo.findById(id);
   }
+
   async updateUser(id, data) {
-  return await userRepo.update(id, data);
+    if (data.password) {
+      const saltRounds = 10;
+      data.password = await bcrypt.hash(data.password, saltRounds);
+    }
+    return await userRepo.update(id, data);
   }
+
   async deleteUser(id) {
-  return await userRepo.softDelete(id);
-}
-
-
+    return await userRepo.softDelete(id);
+  }
 }
 
 module.exports = new UserService();
